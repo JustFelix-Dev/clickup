95555b302a0aea53db5e8b40cdd0927aad581e70
Felix Owolabi <owolabifelix78@gmail.com>
2024-02-11 14:55:58 +0100

    feat: completed parallax section

--- src/components/ParallaxComponent.jsx
+++ src/components/ParallaxComponent.jsx
@@ -20,8 +20,14 @@ const ParallaxComponent = () => {
 
   return (
     <div className="bring-team-container  border ">
-      <div className="flex items-center justify-center circle">
-        <span>Hello World</span>
+      <div className="flex flex-col gap-4 items-center justify-center circle">
+        <span className='bring-teams-text'>Bring teams and work together</span>
+        <p className='text-custom-base text-[20px] font-extrabold'>Eliminate app chaos. Consolidate tools and cut costs.</p>
+        <div className='w-full text-center '>
+        <button className='bring-teams-button shadow-lg hover:scale-95 transition-all'> Get Started</button>
+        <p className='text-[#7F76B3] mt-2 font-medium text-[14px]'>Free forever.No credit card required.</p>
+        <p className='text-[#7F76B3] font-medium text-[14px]'>Join 2+ million teams today!</p>
+        </div>
       </div>
       <div className="content" style={{ backgroundPosition: `-${scrollPosition}px 50%` }}>
       </div>
