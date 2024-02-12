8e51ef3a3de1b81d04a3eb92763941062fab2c8c
Felix Owolabi <owolabifelix78@gmail.com>
2024-02-12 10:52:08 +0100

    feat: added team lookout component

---
+++ src/components/TeamLookOut.jsx
@@ -0,0 +1,10 @@
+
+const TeamLookOut = () => {
+  return (
+    <div className='team-lookout'>
+        <span>Everything your team is looking for</span>
+    </div>
+  )
+}
+
+export default TeamLookOut
