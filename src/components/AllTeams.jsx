78a788df9caa8279a73e6d2a520a9bd21dfd937b
Felix Owolabi <owolabifelix78@gmail.com>
2024-02-12 10:15:39 +0100

    feat: completed the all-team section

--- src/components/AllTeams.jsx
+++ src/components/AllTeams.jsx
@@ -1,6 +1,11 @@
 import finistraRepImage from "../assets/finastra-rep.png";
 import cnRepImage from "../assets/cartoon-network-rep.png";
 import pressedRepImage from "../assets/pressed-rep.png";
+import finistraLogo from "../assets/card-logo-1.svg";
+import cnLogo from "../assets/card-logo-2.svg";
+import pressedLogo from "../assets/card-logo-3.svg";
+
+
 import ImageCard from "./ImageCard";
 
 const AllTeams = () => {
@@ -9,10 +14,10 @@ const AllTeams = () => {
         <header>All teams love ClickUp</header>
         <p className="text-[20px] text-center text-darked font-[700]">Teams love how ClickUp helps them get things done efficiently.</p>
         <p className="text-[20px] text-center text-darked ">But don’t take our word for it, hear it directly from them.</p>
-         <div className="rep-team flex ">
-            <ImageCard image={finistraRepImage} />
-            <ImageCard image={cnRepImage} />
-            <ImageCard image={pressedRepImage} />
+         <div className="rep-team flex w-[90%] mx-auto my-20">
+            <ImageCard image={finistraRepImage} logo={finistraLogo} text='"With ClickUp we&apos;ve seen a 40% improvement in our total go-to-market efficiency!"' />
+            <ImageCard image={cnRepImage} logo={cnLogo} text='"ClickUp is our one source of truth for all the details we need - It helps us work faster."' />
+            <ImageCard image={pressedRepImage} logo={pressedLogo} text='"ClickUp has helped us 3x our productivity without having to scale our team."' />
          </div>
     </div>
   )
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,13 +1,19 @@
+import angledArrow from "../assets/angled-arrow.svg";
 
-const ImageCard = ({image}) => {
+const ImageCard = ({image,logo,text}) => {
   return (
-    <div className="rounded-xl shadow-md overflow-hidden relative w-full max-w-[385px] h-[500px] mx-auto">
+    <div className="rounded-xl shadow-md overflow-hidden relative w-full max-w-[385px] mx-auto">
     <img src={image} alt="image" className="w-full h-auto" />
-    <div className="details absolute  left-0 right-0 bottom-0 text-white text-center  flex items-center justify-center">
-     <p className>Lorem100</p>
+    <div className="details absolute  left-0 right-0 bottom-0 text-white text-center  flex p-8">
+        <div className="text-content flex flex-col gap-4 justify-between">
+            <div className="h-[30px]">
+        <img src={logo} alt="logo" className="self-start logo "  />
+            </div>
+        <p className="font-medium text-[20px] text-left">{text}</p>
+        <button className="read-story"> <span>Read story</span> <img src={angledArrow} alt="angled-arrow" /></button>
+        </div>
     </div>
   </div>
-  
   )
 }
 
