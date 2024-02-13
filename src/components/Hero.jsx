0784c64ccd90d4e96e04f2854ec5349654bfaab2
Felix Owolabi <owolabifelix78@gmail.com>
2024-02-13 19:01:42 +0100

    fix: adjusted hero section styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,15 @@ import reviewsFrom from "../assets/reviews-from.svg";
 const Hero = () => {
   return (
     <div className=" flex justify-center  text-center mt-20">
-      <div className=" text-white flex flex-col items-center gap-2 ">
+      <div className=" text-white flex flex-col items-center gap-6 ">
         <span className="bg-black text-[13px] font-medium rounded-full px-4 py-1">Discover the all-new Clickup 3.0 &rarr;</span>
         <div className=" hero-text">
             One app to replace them all
         </div>
+        <div className="flex flex-col">
         <span className="text-[#24223E] text-[23px] font-bold">Get everyone working in a single platform</span>
         <span className="text-[#24223E] font-medium text-[23px]">designed to manage any type of work.</span>
+        </div>
         <button className="hero-button shadow-lg">Get Started. It&apos;s FREE &rarr;</button>
         <div className="flex gap-1 items-center p-1">
             <img src={fiveStars} alt="five-stars" />
